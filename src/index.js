9452021cb8fc98c1aa79f0970299573e5c0d4a9f
binarycasper <binarycasper@gmail.com>
2019-07-03 21:38:30 -0300

    add starter game files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-const messageEl = document.createElement('div');
-messageEl.textContent = 'Heyo';
-document.body.appendChild(messageEl);
+import 'phaser';
+
+import { playScene } from './scenes/playScene';
+
+export let game;
+
+export let gameOptions = {
+    platformStartSpeed: 350,
+    spawnRange: [100, 350],
+    platformSizeRange: [50, 250],
+    playerGravity: 900,
+    jumpForce: 400,
+    playerStartPosition: 200,
+    jumps: 2
+}
+
+window.onload = function() {
+    let gameConfig ={
+        type: Phaser.AUTO,
+        width: 1334,
+        height: 750,
+        scene: playScene,
+        backgroundColor: 0x444444,
+
+        physics: {
+            default: "arcade"
+        }
+    }
+
+    game = new Phaser.Game(gameConfig);
+    window.focus();
+    resize();
+    window.addEventListener("rezise", resize, false);
+}
+
+function resize(){
+    let canvas = document.querySelector("canvas");
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let windowRatio = windowWidth/windowHeight;
+    let gameRatio = game.config.width/game.config.height;
+    if(windowRatio<gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth/gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px"; 
+    }
+}
